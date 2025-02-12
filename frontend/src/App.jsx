6e33b151128ef90c2e4291bd51126981deb4dc85
ongsici <ongsici@gmail.com>
2025-02-12 22:46:28 +0000

    feat: initial commit for frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
+
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        {/* <Route path="/" element={<Layout />}> */}
+          <Route path="/" element={<Home />} />
+        {/* </Route> */}
+      </Routes>
+    </Router>
+
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/Home/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css"; // Optional, for styles
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <h1>Tether</h1>
+      <p>Travel Together, Connecting You to the World</p>
+    </div>
+  );
+};
+
+export default Home;
