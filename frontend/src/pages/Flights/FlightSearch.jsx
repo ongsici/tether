886edc5c43b95f1475a710ea558d81b3884df8dd
ongsici <ongsici@gmail.com>
2025-02-25 22:57:51 +0000

    chore: testing payload update with userid

--- frontend/src/pages/Flights/FlightSearch.jsx
+++ frontend/src/pages/Flights/FlightSearch.jsx
@@ -66,7 +66,7 @@ function Flights() {
     <Container maxWidth="md" sx={{ mt: 6, textAlign: "center" }} className="home-container">
       <div className="background-overlay"></div>
       <Box className="content-box">
-      {!user ? (
+      {user ? (
         <>
           <Typography variant="h4" className="page-title" sx={{ fontFamily: 'Roboto, sans-serif', fontWeight: 700 }}>Plan Your Travel</Typography>
 
