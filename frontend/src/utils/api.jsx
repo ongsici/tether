fc10971d576968f172b73708d972b19312198179
ongsici <66452702+ongsici@users.noreply.github.com>
2025-02-26 18:13:07 +0000

    Frontend (#10)
    * chore: added cities
    
    * fix: set city use state removed
    
    * fix: incorrect saving of city options with code
    
    * fix: text input on cities not showing up correctly
    
    * feat: date restrictions and fixed cities display for api call
    
    * chore: moved api to separate utils
    
    * chore: refactor using hooks and added api call to flights page
    
    * chore: refactored itinerary and weather page to use hooks and helpers
    
    * feat: flight results page and fix weather issues
    
    * chore: updated styling for flight results page
    
    * feat: home results page
    
    * feat: about us page
    
    * chore: styling
    
    * chore: updated flight search cities and styling
    
    * feat: weather results page
    
    * fix: frontend display for flight search results
    
    * chore: updated api payload
    
    * chore: testing user details
    
    * chore: testing updated payload with userid
    
    * chore: testing payload update with userid
    
    * feat: itinerary results page and userid for other pages
    
    * feat: toast message for success and error
    
    * feat: save flights option with toast

--- frontend/src/utils/api.jsx
+++ frontend/src/utils/api.jsx
@@ -1,4 +1,5 @@
 const APIM_URL="http://localhost:8000/api/submitData";
+const APIM_SAVE_URL="http://localhost:8000/api/saveData";
 
 
 export const searchTravel = async (requestBody) => {
@@ -18,4 +19,25 @@ export const searchTravel = async (requestBody) => {
       console.error("API Error:", error);
       return null;
     }
+
   };
+
+export const saveFlight = async (requestBody) => {
+    try {
+      const response = await fetch(`${APIM_SAVE_URL}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(requestBody),
+      });
+      if (!response.ok) {
+        throw new Error("Failed to save flight");
+      }
+      return { success: true, message: "Flight saved successfully!" };
+    } catch (error) {
+      console.error("Error saving flight:", error);
+      return { success: false, message: "Error saving flight." };
+    }
+  };
+  
