61a44cc1ceb90bd66cd12b76ef2855cd6a238f32
ongsici <ongsici@gmail.com>
2025-02-26 18:01:55 +0000

    feat: save flights option with toast

--- frontend/src/utils/api.jsx
+++ frontend/src/utils/api.jsx
@@ -1,4 +1,5 @@
 const APIM_URL="http://localhost:8000/api/submitData";
+const APIM_SAVE_URL="http://localhost:8000/api/saveData";
 
 
 export const searchTravel = async (requestBody) => {
@@ -19,3 +20,23 @@ export const searchTravel = async (requestBody) => {
       return null;
     }
   };
+
+  export const saveFlight = async (requestBody) => {
+    try {
+      const response = await fetch(`${APIM_SAVE_URL}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(requestBody),
+      });
+      if (!response.ok) {
+        throw new Error("Failed to save flight");
+      }
+      return { success: true, message: "Flight saved successfully!" };
+    } catch (error) {
+      console.error("Error saving flight:", error);
+      return { success: false, message: "Error saving flight." };
+    }
+  };
+  
