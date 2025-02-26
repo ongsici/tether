cf2664d5063c39cf572ed3758463defa5799c6d2
gphang <gphang@umich.edu>
2025-02-26 17:09:09 +0000

    fix: using POST instead of GET

--- itinerary-app/README.md
+++ itinerary-app/README.md
@@ -6,5 +6,15 @@ uvicorn app:app --reload
 docker pull ghcr.io/gphang/itinerary-app:latest
 docker run -d -p 7000:7000 -e OPENWEATHER_KEY="insert_key" -e AMADEUS_KEY="insert_key" -e AMADEUS_SECRET="insert_secret" ghcr.io/gphang/itinerary-app:latest
 '''
-sample link when docker is running:
-http://127.0.0.1:7000/itinerary?city=London&radius=10&limit=2
+sample request when docker is running:
+curl -X 'POST' \
+  'http://127.0.0.1:7000/itinerary' \
+  -H 'Content-Type: application/json' \
+  -d '{
+    "user_id": "5",
+    "itinerary": {
+      "city": "London",
+      "radius": 10,
+      "limit": 5
+    }
+  }'
--- itinerary-app/app.py
+++ itinerary-app/app.py
@@ -10,47 +10,35 @@ logger = logging.getLogger("itinerary_microservice")
 
 app = FastAPI()
 
-# endpoint to fetch activities for the given city and optional parameters
-@app.get("/itinerary", response_model=ItineraryResponse)
-async def fetch_itinerary(user_id: str, city: str, radius: int = 10, limit: int = 5):
+# endpoint to fetch activities for the given user_id, city, radius, limit
+@app.post("/itinerary", response_model=ItineraryResponse)  # Use POST method here
+async def fetch_itinerary(request: ItineraryRequest):
     try:
+        city = request.itinerary.city
+        radius = request.itinerary.radius
+        limit = request.itinerary.limit
+
         logger.info(f"Fetching activities for city: {city}")
-        activities = get_city_activities(user_id, city, radius, limit)
+        activities = get_city_activities(request.user_id, city, radius, limit)
         logger.info(f"Itinerary data fetched successfully for city: {city}")
-        
+
         return activities
     
     except Exception as e:
         logger.error(f"Error fetching itinerary data: {str(e)}")
         raise HTTPException(status_code=400, detail=str(e))
 
-# sample GET request: http://127.0.0.1:7000/itinerary?city=London&radius=10&limit=2
-
-
-# @app.post("/itinerary", response_model=List[ItineraryResponse])  # Use POST method here
-# async def fetch_itinerary(request: ItineraryRequest):
+# @app.get("/itinerary", response_model=ItineraryResponse)
+# async def fetch_itinerary(user_id: str, city: str, radius: int = 10, limit: int = 5):
 #     try:
-#         city = request.city
-#         radius = request.radius
-#         limit = request.limit
-
 #         logger.info(f"Fetching activities for city: {city}")
-#         activities = get_city_activities(city, radius, limit)
+#         activities = get_city_activities(user_id, city, radius, limit)
 #         logger.info(f"Itinerary data fetched successfully for city: {city}")
-
-#         return activities  # Return the list of ItineraryResponse objects
+        
+#         return activities
     
 #     except Exception as e:
 #         logger.error(f"Error fetching itinerary data: {str(e)}")
 #         raise HTTPException(status_code=400, detail=str(e))
 
-
-# sample POST request:
-# curl -X 'POST' \
-#   'http://127.0.0.1:7000/itinerary' \
-#   -H 'Content-Type: application/json' \
-#   -d '{
-#   "city": "London",
-#   "radius": 10,
-#   "limit": 2
-# }'
+# sample GET request: http://127.0.0.1:7000/itinerary?user_id=gphang&city=London&radius=10&limit=2
