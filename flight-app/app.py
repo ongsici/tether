d0ef1c1d282de88b165db63ea0618a46533dfcc4
ongsici <66452702+ongsici@users.noreply.github.com>
2025-02-27 21:20:39 +0000

    Flight app (#14)
    * flight microservice attempt
    
    * chore: added Dockerfile and updated utils
    
    * fix logging issue
    
    * chore: update
    
    * feat: change structure of response and dockerfile
    
    * fix: change names for flight response and request
    
    * chore: added yml file for pipeline deploy in weather-app
    
    * feat: changed structure of flight response
    
    * feat: working locally
    
    * fix: working on azure
    
    * Frontend (#9)
    
    * chore: added cities
    
    * fix: set city use state removed
    
    * fix: incorrect saving of city options with code
    
    * fix: text input on cities not showing up correctly
    
    * feat: date restrictions and fixed cities display for api call
    
    * chore: moved api to separate utils
    
    * chore: refactor using hooks and added api call to flights page
    
    * chore: refactored itinerary and weather page to use hooks and helpers
    
    * feat: flight results page and fix weather issues
    
    * chore: updated styling for flight results page
    
    * feat: home results page
    
    * feat: about us page
    
    * chore: styling
    
    * chore: updated flight search cities and styling
    
    * feat: weather results page
    
    * fix: frontend display for flight search results
    
    * chore: updated api payload
    
    * chore: testing user details
    
    * chore: testing updated payload with userid
    
    * chore: testing payload update with userid
    
    * feat: itinerary results page and userid for other pages
    
    * Update weather-app.yml
    
    * Frontend (#10)
    
    * chore: added cities
    
    * fix: set city use state removed
    
    * fix: incorrect saving of city options with code
    
    * fix: text input on cities not showing up correctly
    
    * feat: date restrictions and fixed cities display for api call
    
    * chore: moved api to separate utils
    
    * chore: refactor using hooks and added api call to flights page
    
    * chore: refactored itinerary and weather page to use hooks and helpers
    
    * feat: flight results page and fix weather issues
    
    * chore: updated styling for flight results page
    
    * feat: home results page
    
    * feat: about us page
    
    * chore: styling
    
    * chore: updated flight search cities and styling
    
    * feat: weather results page
    
    * fix: frontend display for flight search results
    
    * chore: updated api payload
    
    * chore: testing user details
    
    * chore: testing updated payload with userid
    
    * chore: testing payload update with userid
    
    * feat: itinerary results page and userid for other pages
    
    * feat: toast message for success and error
    
    * feat: save flights option with toast
    
    * Itinerary app (#11)
    
    * feat: ollama api for itinerary planning (for single city)
    
    * code for amadeus itinerary
    
    * feat: Dockerfile + start.sh files for Docker deployment
    
    * ops: variable names for Azure deployment
    
    * fix: return activity_id
    
    * feat: new itinerary requests/response classes
    
    * fix: using POST instead of GET
    
    ---------
    
    Co-authored-by: gphang <gphang@umich.edu>
    
    * fix: only returns 1 picture
    
    * Weather app (#12)
    
    * docs: Dockerfile, README
    
    * refactor: WeatherResponse model
    
    * build: weather-app workflow, fix: post method
    
    * fix: weather-app workflow
    
    ---------
    
    Co-authored-by: Ashley Chung <ashleychung@Ashleys-MacBook-Pro-2.local>
    
    * chore: updated state management using context api and local browser storage (#13)
    
    * added tests and fixed return structure
    
    * fix: fix test
    
    ---------
    
    Co-authored-by: Jingxuan Zhang <zjxuan@Jingxuans-MacBook-Pro.local>
    Co-authored-by: Fiona Zhang <fiona.zhang21@imperial.ac.uk>
    Co-authored-by: gphang <gphang@umich.edu>
    Co-authored-by: Ashley Chung <ashleychung@Ashleys-MacBook-Pro-2.local>

---
+++ flight-app/app.py
@@ -0,0 +1,46 @@
+from fastapi import FastAPI, HTTPException
+from src.utils.custom_logging import configure_logging
+from src.services.flight_service import get_flights
+from src.models.flight_model import FlightRequest, FlightResponse
+import logging
+
+configure_logging()
+logger = logging.getLogger("flight_microservice")
+
+app = FastAPI()
+
+@app.post("/flight", response_model=FlightResponse)
+async def fetch_flight(request: FlightRequest):
+    origin_loc = request.flights.origin_loc_code
+    dest_loc = request.flights.destination_loc_code
+    dep_date = request.flights.departure_date
+    ret_date = request.flights.return_date
+    num_passengers = request.flights.num_passenger
+    
+    try:
+        logger.info(f"Fetching flight data from {origin_loc} to {dest_loc} from {dep_date} to {ret_date}")
+        flight_data = get_flights(origin_loc, dest_loc, num_passengers, dep_date, ret_date, request.user_id)
+        logger.info(f"Flight data fetched successfully from {origin_loc} to {dest_loc}")
+
+        return flight_data
+    
+    except Exception as e:
+        logger.error(f"Error fetching flight data: {str(e)}")
+        raise HTTPException(status_code=400, detail=str(e))
+
+
+# sample POST request:
+
+# curl -X POST \
+#      -H "Content-Type: application/json" \
+#      -d '{
+#          "flights": {
+#              "origin_loc_code": "SYD",
+#              "destination_loc_code": "SIN",
+#              "departure_date": "2025-03-10",
+#              "return_date": "2025-03-17",
+#              "num_passenger": 1
+#          },
+#          "user_id": "testuser123"
+#      }' \
+#      http://0.0.0.0:9000/flight
---
+++ flight-app/src/utils/api_client.py
@@ -0,0 +1,30 @@
+import requests
+from .api_token_refresh import get_valid_token
+
+# Amadeus API, flight offers
+def get_flight_data(origin_loc_code: str, destination_loc_code: str, num_passenger: str, 
+                    departure_date: str, return_date: str) -> dict:
+    AMADEUS_URL = f"https://test.api.amadeus.com/v2/shopping/flight-offers?originLocationCode={origin_loc_code}&destinationLocationCode={destination_loc_code}&departureDate={departure_date}&returnDate={return_date}&adults={num_passenger}&max=5"
+
+    headers = {
+        "Authorization": f"Bearer {get_valid_token()}"
+    }
+
+    response = requests.get(AMADEUS_URL, headers=headers)
+    if response.status_code == 401:
+        print("Token expired, refreshing...")
+        headers["Authorization"] = f"Bearer {get_valid_token()}"
+        response = requests.get(AMADEUS_URL, headers=headers)
+    elif response.status_code == 200:
+        print(response.text)
+        data = response.json()
+        return data
+    else:
+        print(f"Error {response.status_code}: {response.json()}")
+        response.raise_for_status()
+
+
+
+
+
+
