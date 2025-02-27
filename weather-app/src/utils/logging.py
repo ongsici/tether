24b53b4fce7458e2d3563f7cc13dcb3f3f39b997
ongsici <66452702+ongsici@users.noreply.github.com>
2025-02-27 16:40:57 +0000

    Weather app (#12)
    * docs: Dockerfile, README
    
    * refactor: WeatherResponse model
    
    * build: weather-app workflow, fix: post method
    
    * fix: weather-app workflow
    
    ---------
    
    Co-authored-by: Ashley Chung <ashleychung@Ashleys-MacBook-Pro-2.local>

--- weather-app/src/utils/logging.py
+++ weather-app/src/utils/logging.py
@@ -2,9 +2,9 @@ import logging
 import sys
 from pathlib import Path
 
-LOG_DIR = Path("logs")
-LOG_FILE = LOG_DIR / "weather_microservice.log"
-LOG_DIR.mkdir(exist_ok=True) # create log directory if it doesn't exist
+# LOG_DIR = Path("logs")
+# LOG_FILE = LOG_DIR / "weather_microservice.log"
+# LOG_DIR.mkdir(exist_ok=True) # create log directory if it doesn't exist
 
 def configure_logging():
     log_format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
@@ -17,9 +17,9 @@ def configure_logging():
     console_handler.setFormatter(formatter)
     logger.addHandler(console_handler)
 
-    file_handler = logging.FileHandler(LOG_FILE)
-    file_handler.setFormatter(formatter)
-    logger.addHandler(file_handler)
+    # file_handler = logging.FileHandler(LOG_FILE)
+    # file_handler.setFormatter(formatter)
+    # logger.addHandler(file_handler)
 
     # limit verbose logs from external libraries
     logging.getLogger("requests").setLevel(logging.WARNING)
