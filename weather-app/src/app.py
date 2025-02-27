ff575ffff9cb452bc94bc6c44afd73129d5d45eb
Ashley Chung <ashleychung@Ashleys-MacBook-Pro-2.local>
2025-02-27 16:55:47 +0000

    fix: cors config

--- weather-app/src/app.py
+++ weather-app/src/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from .models.weather_model import WeatherRequest, WeatherResponse
 from .services.weather_service import get_weather
 from .utils.logging import configure_logging
@@ -8,6 +9,14 @@ configure_logging()
 logger = logging.getLogger("weather_microservice")
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://tether-apim-2.azure-api.net", "http://localhost:8000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # endpoint to fetch weather data for given city and optional country code
 @app.post("/weather", response_model=WeatherResponse)
