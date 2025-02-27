b055632648a6e93a93594f6fa8029ff9540c44dc
gphang <gphang@umich.edu>
2025-02-27 19:47:15 +0000

    fix: variable names for Azure

--- db-app/src/utils/api_client.py
+++ db-app/src/utils/api_client.py
@@ -7,11 +7,11 @@ from sqlalchemy.orm import sessionmaker
 load_dotenv()
 
 # db configuration
-DB_HOST = os.getenv("DB_HOST", "tether-database.postgres.database.azure.com")
-DB_NAME = os.getenv("DB_NAME", "your_database_name")
-DB_USER = os.getenv("DB_USER", "your_username")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "your_password")
-DB_PORT = os.getenv("DB_PORT", "5432")
+DB_HOST = os.getenv("DB-HOST", "tether-database.postgres.database.azure.com")
+DB_NAME = os.getenv("DB-NAME", "your_database_name")
+DB_USER = os.getenv("DB-USER", "your_username")
+DB_PASSWORD = os.getenv("DB-PASSWORD", "your_password")
+DB_PORT = os.getenv("DB-PORT", "5432")
 
 DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
