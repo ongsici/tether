64450c6dc585a08ca6eb9e84df5c9554e697d3e8
ongsici <ongsici@gmail.com>
2025-03-04 12:37:46 +0000

    fix: use fetch url reading

--- frontend/src/hooks/useFetchCities.jsx
+++ frontend/src/hooks/useFetchCities.jsx
@@ -3,14 +3,14 @@ import { useEffect, useState } from "react";
 const useFetchCities = (url) => {
     const [cities, setCities] = useState([]);
   
-    useEffect((url) => {
+    useEffect(() => {
+      if (!url) return; 
+  
       fetch(url)
         .then((response) => response.json())
-        .then((data) => {
-          setCities(data);
-        })
+        .then((data) => setCities(data))
         .catch((error) => console.error("Error fetching cities:", error));
-    }, []);
+    }, [url]); 
   
     return cities;
   };
